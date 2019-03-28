d1d26a7cf2c8c7d1e06529d1a58b95d6c2b0af29
Jacob Slusser <jslusser33@gmail.com>
2019-03-28 13:19:57 -0500

    Added comments

--- PayRate.py
+++ PayRate.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+#Set the variables
 PayRate = float(input("Enter pay rate "))
 Hours = float(input("Enter amount of hours worked "))
 
+#Calculate payrate depending on user input of hours worked and hourly wages
 if PayRate < 10 or Hours > 40:
     OvertimeHours = (Hours - 40)
     OvertimePay = (OvertimeHours * 1.5 * PayRate)
