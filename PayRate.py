8771bba66f68d2b9e4df77e8448aac01908710a0
Jacob Slusser <jslusser33@gmail.com>
2019-03-28 08:41:41 -0500

    First commit to my school work repository

---
+++ PayRate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+PayRate = float(input("Enter pay rate "))
+Hours = float(input("Enter amount of hours worked "))
+
+if PayRate < 10 or Hours > 40:
+    OvertimeHours = (Hours - 40)
+    OvertimePay = (OvertimeHours * 1.5 * PayRate)
+    TotalPay = (40 * PayRate + OvertimePay)
+    print ("Total pay is "+str(round(TotalPay,2)))
+else:
+    TotalPay = (Hours * PayRate)
+    print ("Total pay is "+str(round(TotalPay,2)))
+
+    
